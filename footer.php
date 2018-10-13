be6f3d49f53484b2b363449fd6127584ebcefae4
Eric Li <ericut@gmail.com>
2018-10-13 12:25:45 -0300

    [V 0.0.1] MDB for WP

---
+++ footer.php
@@ -0,0 +1,12 @@
+		<?php 
+			wp_footer(); 
+			$wp_theme_url = get_template_directory_uri();
+		?>
+
+		<script type="text/javascript" src="<?= $wp_theme_url; ?>/js/jquery-3.3.1.min.js"></script>
+		<script type="text/javascript" src="<?= $wp_theme_url; ?>/js/popper.min.js"></script>
+		<script type="text/javascript" src="<?= $wp_theme_url; ?>/js/bootstrap.min.js"></script>
+		<script type="text/javascript" src="<?= $wp_theme_url; ?>/js/mdb.min.js"></script>
+	</body>
+
+</html>
---
+++ header.php
@@ -0,0 +1,31 @@
+<?php
+$wp_theme_url = get_template_directory_uri();
+?>
+
+<!doctype html>
+<html lang="pt_BR">
+
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<meta http-equiv="x-ua-compatible" content="ie=edge">
+		<title>Duo JK Square</title>
+		<meta name="description" content="Duo JK Square">
+		<meta name="author" content="Duo JK Square">
+
+		<link rel="stylesheet" href="<?= $wp_theme_url; ?>/style.css">
+
+		<link rel="stylesheet" href="<?= $wp_theme_url; ?>/css/bootstrap.css">
+		<link rel="stylesheet" href="<?= $wp_theme_url; ?>/css/mdb.css">
+		<link rel="stylesheet" href="<?= $wp_theme_url; ?>/css/style.css">
+
+		<link rel="stylesheet" href="<?= $wp_theme_url; ?>/css/lp.css">
+
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+
+		<?php  wp_head(); ?>
+	</head>
+
+	<body>
+
+
